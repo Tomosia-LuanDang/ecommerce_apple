795bade73af2bce76889465986ca4911d93524a1
Tomosia-LuanDang <tt.luan.dang@tomosia.com>
2022-06-01 17:02:53 +0700

    Sign up with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,8 @@
-class User < ApplicationRecord;end
+  class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role:   { admin: 1, user: 2 }
+  enum gender: { male: true, female: false }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -116,18 +116,20 @@ ActiveRecord::Schema.define(version: 2022_05_30_090655) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "phone"
-    t.string "password_digest"
-    t.string "remember_digest"
     t.boolean "gender"
-    t.integer "role"
-    t.boolean "is_active"
-    t.string "activation_digest"
-    t.string "reset_digest"
+    t.integer "role", default: 2, null: false
     t.string "avatar"
+    t.boolean "is_active"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "assets", "products"
