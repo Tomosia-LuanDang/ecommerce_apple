be14109ad3864410e92f628bdec352d44d8d8f57
luandang4 <luandang4@gmail.com>
2022-06-18 19:55:46 +0700

    Remake seed_fu

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,23 +1,23 @@
 User.delete_all
+password = "123123"
 User.create!(
   email:                 "luan@gmail.com",
   name:                  "Luan Dang",
-  phone:                 "0389 456 666",
+  phone:                 "0389456666",
   gender:                true,
   role:                  1,
   avatar:                "varun.jpg",
   is_active:             true,
-  password:              "123123",
-  password_confirmation: "123123",
+  password:              password,
+  password_confirmation: password,
   created_at:            Time.zone.now
 )
 
-30.times do |n|
-  password = "123123"
+20.times do |n|
   User.create!(
     email:                 Faker::Internet.email,
     name:                  Faker::Name.name,
-    phone:                 Faker::PhoneNumber.cell_phone,
+    phone:                 ["+84","0"].sample + ["3","5","7","8","9"].sample + '%08d' % rand(8 ** 8),
     gender:                Faker::Gender.binary_type.downcase,
     role:                  0,
     avatar:                "varun.jpg",
